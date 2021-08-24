0fec7898009f3c37f6acb049aa95b864d6fc38d4
J8 <67118395+j8source@users.noreply.github.com>
2021-08-24 17:47:23 +0800

    Create movie_list.php

---
+++ v/data/movie_list.php
@@ -0,0 +1,41 @@
+<?php $listArr=array (
+  0 => 
+  array (
+    'vid' => 141,
+    'name' => '换妻俱乐部',
+    'img' => 'https://www.j8nb.com/img/uploads/v/20200620/201802161518765681.jpg',
+    'desc' => '迪亚娜和她的丈夫于连厌倦了枯燥的夫妻生活，决定尝试与另一对夫妇进行换.妻游戏，他们邀请亚历克斯和迪穆夫妇在周末到他们的住所进行试验。亚历克斯似乎很有信心也很乐意，而迪穆则很奇怪并不感兴趣。夜幕降临，他们的性探索开始...',
+    'url' => 
+    array (
+      1 => 'https://out.ucxiazai.com/2020/06/16/g4mauJCt8oTQIqhi/index.m3u8',
+      2 => 'https://209zy.suyunbo.tv/2020/06/16/y2s9j44oR6NPuMAw/playlist.m3u8',
+      5 => 'https://eth.ppzuida.com/20171126/ITw211Nt/index.m3u8',
+    ),
+  ),
+  1 => 
+  array (
+    'vid' => 177,
+    'name' => '漂流欲室',
+    'img' => 'https://www.j8nb.com/img/uploads/v/20200621/177.jpg',
+    'desc' => '《漂流欲室》是由金基德执导，徐情、金儒哲主演的韩国情色电影。影片讲述了杀人的前警察与哑巴妓女之间的爱欲情仇..Drifting Room 은 Kim Kidd가 감독하고 Xu Qing과 Jin Ruzhe가 주연 한 한국의 에로틱 한 영화입니다. 이 영화는 살해 전 경찰과 멍청한 매춘부 사이의 에로틱 한 불화에 대해 알려줍니다.',
+    'url' => 
+    array (
+      1 => 'https://sohu.com-v-sohu.com/20181018/13635_04c72d31/index.m3u8',
+      2 => 'https://yiqikan.wuyouzuida.com/20200426/4492_d000df38/index.m3u8',
+      3 => 'https://videos.kkyun-iqiyi.com/20180803/4wupXyrf/index.m3u8',
+    ),
+  ),
+  2 => 
+  array (
+    'vid' => 192,
+    'name' => '爱情的限度[法]',
+    'img' => 'https://www.j8nb.com/img/uploads/v/20200621/192.jpg',
+    'desc' => '脆弱敏感的英俊青年皮埃尔（Louis Garrel 路易斯·加瑞尔 饰）时隔多年终于回到父母的身边，他对父母尤其是母亲海伦（Isabelle Huppert 伊莎贝尔·于佩尔 饰）表现出了过分的依恋...Le jeune homme fragile et sensible Pierre (Louis Garrel Louis Garrel) est finalement revenu auprès de ses parents après de nombreuses années. Attachement',
+    'url' => 
+    array (
+      3 => 'https://youku.kuyun-leshi.com/20190301/P6LJet4R/index.m3u8',
+      4 => 'https://eth.ppzuida.com/20180827/wGFIgXCW/index.m3u8',
+    ),
+  ),
+)
+?>
